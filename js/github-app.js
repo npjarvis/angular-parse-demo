80a73d05fd834e27bee3ef63657fae191506db3d
npjarvis <npjarvis@gmail.com>
2014-11-05 11:16:12 -0800

    added stuff like loading and stuff

--- js/github-app.js
+++ js/github-app.js
@@ -8,11 +8,22 @@
 
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
-        $scope.userName = 'drstearns';
+    .controller('GitHubController', function($scope, $http) {
+        $scope.userName = 'npjarvis';
         $scope.getRepos = function() {
-
-            //add code here
-
+    // this makes request to ajax and returns data to us
+            $scope.loading = true;
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    $scope.repos = data;
+                    $scope.errorMessage = null;
+                })
+                .error(function(err) {
+                   // alert('User does not exist');
+                    $scope.errorMessage = err.message || 'User does not exist!';
+                })
+                .finally(function () {
+                    $scope.loading = false;
+                });
         };
     });
